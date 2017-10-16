d2ff037ecc5514a0324e31f0f18e78a0ef0c592a
JR-SWJTU <752258828@qq.com>
2017-10-16 20:27:33 +0800

    [add] add the test entity

---
+++ src/main/java/com/swjtu/roadCheck/mapper/ItemsMapperCustom.java
@@ -0,0 +1,32 @@
+package com.swjtu.roadCheck.mapper;
+
+
+
+import com.swjtu.roadCheck.entity.ItemsCustom;
+import com.swjtu.roadCheck.entity.ItemsQueryVo;
+
+import java.util.List;
+
+
+/**
+ * 
+ * <p>
+ * Title: ItemsMapperCustom
+ * </p>
+ * <p>
+ * Description:商品自定义mapper
+ * </p>
+ * <p>
+ * Company: www.itcast.com
+ * </p>
+ * 
+ * @author 传智.燕青
+ * @date 2015-3-20下午3:00:53
+ * @version 1.0
+ */
+public interface ItemsMapperCustom {
+	// 商品查询列表
+	List<ItemsCustom> findItemsList(ItemsQueryVo itemsQueryVo)
+			throws Exception;
+
+}
---
+++ src/main/java/com/swjtu/roadCheck/mapper/UserMapper.java
@@ -0,0 +1,31 @@
+package com.swjtu.roadCheck.mapper;
+
+import java.util.List;
+
+import com.swjtu.roadCheck.entity.User;
+import com.swjtu.roadCheck.entity.UserExample;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserMapper {
+    int countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
