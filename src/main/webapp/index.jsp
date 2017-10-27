737180cf6a9220f1ebe5994fb06dc53cd37b5b8e
TR <244374078@qq.com>
2017-10-27 16:42:47 +0800

    地图样式初版备份

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -139,6 +139,28 @@
                 <div id="spaceMap" class="map">
 
                 </div>
+                <div style="position: absolute;left:300px;top:50px;height: 50px;">
+                    <div class="barchild">
+                        <div style="margin:auto;width:30px;height:20px;background: red;"></div>
+                        <p style="margin:auto;text-align: center;color: white">总数</p>
+                    </div>
+                    <div class="barchild" style="left: 50px;">
+                        <div style="margin:auto;width:30px;height:20px;background: green;"></div>
+                        <p style="margin:auto;text-align: center;color: white;">财损</p>
+                    </div>
+                    <div class="barchild" style="left: 90px;">
+                        <div style="margin:auto;width:30px;height:20px;background: blue;"></div>
+                        <p style="margin:auto;text-align: center;color: white;">轻伤</p>
+                    </div>
+                    <div class="barchild" style="left: 130px;">
+                        <div style="margin:auto;width:30px;height:20px;background: yellow;"></div>
+                        <p style="margin:auto;text-align: center;color: white;">重伤</p>
+                    </div>
+                    <div class="barchild" style="left: 170px;">
+                        <div style="margin:auto;width:30px;height:20px;background: olive;"></div>
+                        <p style="margin:auto;text-align: center;color: white;">死亡</p>
+                    </div>
+                </div>
             </div>
         </div>
         <div v-show="nowFuc == 'time'" :style="bodyContent">
@@ -175,6 +197,7 @@
                 <%--<mu-icon-button tooltip="筛选数据重置" tooltip-position="bottom-right" icon="grade" class="right-btn" @click="timeGet"></mu-icon-button>--%>
             </div>
             <div class="body-right" :style="mapStyle">
+
                 <div id="timeMap" class="map">
 
                 </div>
--- src/main/webapp/js/map.js
+++ src/main/webapp/js/map.js
@@ -16,14 +16,52 @@ var timeMap = new AMap.Map('timeMap', {
     city:'成都',
     center:[104.082584,30.682064]
 });
+//路段参数
+var markerOptions = {
+    tyep:0,//类型0路段，1交叉口
+    name:"",//名称
+    lat:0.0,//经纬度
+    lng:0.0,//经纬度
+    accidentNumbers:0,//事故总数
+    wealthLoss:0,//财损数量
+    slightInjury:0,//轻伤数量
+    seriousInjury:0,//重伤数量
+    death:0,//死亡数量
+}
 
+// function markerOptions(tyep,name,lat,lng,accidentNumbers,wealthLoss,slightInjury,seriousInjury,death) {
+//         this.tyep              = tyep                //类型0路段，1交叉口
+//         this.name              = name                //名称
+//         this.lat               = lat                 //经纬度
+//         this.lng               = lng                 //经纬度
+//         this.accidentNumbers  = accidentNumbers     //事故总数
+//         this.wealthLoss       =  wealthLoss          //财损数量
+//         this.slightInjury     = slightInjury        //轻伤数量
+//         this.seriousInjury    = seriousInjury       //重伤数量
+//         this.death             = death               //死亡数量
+// }
 
-
+// var m1 = new markerOptions();
+//
+// var m2 = new Object();
+// m2.tyep = 0;
+// m2.name = "";
+// m2.lat = 0.0;
+// m2.lng = 0.0;
+// m2.accidentNumbers = "";
+// m2.wealthLoss = "";
+// m2.slightInjury = "";
+// m2.seriousInjury = "";
+// m2.death = "";
+//
+// console.log(m2.tyep)
 /*
 * 地图操作
 * */
 //spaceMap.setMapStyle('amap://styles/'+"dark");
-spaceMap.setMapStyle('amap://styles/'+"blue");
+spaceMap.setMapStyle('amap://styles/'+"dark");
+blackPointMap.setMapStyle('amap://styles/'+"dark");
+timeMap.setMapStyle('amap://styles/'+"dark");
 
 var features = ['road'];
 spaceMap.setFeatures(features);
@@ -41,14 +79,15 @@ spaceMap.setFeatures(features);
 * */
 /*
 * 设置marker
-* 传入参数为，地图，标志名称，位置（经纬度），类型（0交叉口还是1路段），数量
+* 传入参数为，地图，标志名称，位置（经纬度），类型（0交叉口还是1路段），当前路段|交叉口事故数量
 * */
-addMarker(spaceMap,"XXX路段1",[104.042584,30.582064],1,3)
-addMarker(spaceMap,"XXX交叉口2",[104.072584,30.472064],0,100)
-addMarker(spaceMap,"XXX路段3",[104.092584,30.692064],1,50)
 
-addMarker(blackPointMap,"XXX路段",[104.072584,30.472064],0,100)
-addMarker(blackPointMap,"XXX交叉口3",[104.092584,30.692064],1,50)
+// addMarker(spaceMap,"XXX路段1",[104.042584,30.582064],1,3)
+// addMarker(spaceMap,"XXX交叉口2",[104.072584,30.472064],0,100)
+// addMarker(spaceMap,"XXX路段3",[104.092584,30.692064],1,50)
+//
+// addMarker(blackPointMap,"XXX路段",[104.072584,30.472064],0,100)
+// addMarker(blackPointMap,"XXX交叉口3",[104.092584,30.692064],1,50)
 var mapMarkers = [];
 if (mapMarkers == null || mapMarkers.length == 0) {
     mapMarkers = [];
@@ -57,30 +96,59 @@ if (mapMarkers == null || mapMarkers.length == 0) {
 /*
 * 设置交叉口事故数大小
 *传入参数为数量,全部事故数量
+*
+* amap,tyep,name,lat,lng,accidentNumbers,accidentAllNumbers,wealthLoss,slightInjury,seriousInjury,death
 * */
-function setIntersectionContent(number,allNumbers,name) {
-    var content = "<div style=\"height: 150px;width: 100px\">\n" +
-        "<div  style=\"margin: auto;width: "+getHeight(number,allNumbers)/2+"px;height: "+getHeight(number,allNumbers)/2+"px;background: "+getColor(number,allNumbers)+";border-radius:50%\"></div>" +
-        "    <p style=\"width:100%;margin: auto;text-align: center;color:white;\">事故数："+number+"</p>\n" +
-        "    <p style=\"width:100%;margin: auto;background:white;text-align: center;\">"+name+"</p>\n" +
-        "</div>";
-    return content;
-}
-
-function setRoadContent (number,allNumbers,name) {
-    var content = "<div style=\"height: 200px;width: 100px\">\n" +
-        "    <div id=\"bar\" style=\"width: 20px;height: "+getHeight(number,allNumbers)+"px;background: "+getColor(number,allNumbers)+"; margin: auto\"></div>\n" +
-        "    <p style=\"width:100%;margin: auto;text-align: center;color: "+"white"+"\">事故数："+number+"</p>\n" +
-        "    <p style=\"width:100%;margin: auto;background:white;text-align: center;\">"+name+"</p>\n" +
-        "</div>";
-    return content;
+var markerOptions = {
+    map                  : spaceMap,            //地图
+    type                 : 0           ,   //类型1路段，0交叉口
+    name                 : "交叉口1"           ,   //名称
+    lat                  : 104.072584            ,   //经纬度
+    lng                  : 30.472064            ,   //经纬度
+    isChenDu            : false,                   //是否为严重程度展示
+    showType            : "财损,轻伤"     ,   //严重程度需要的展示内容
+    accidentAllNumbers : 200,  //所有事故总数
+    accidentNumbers     : 100 ,  //当前路段/交叉口事故总数
+    wealthLoss          : 30     ,   //财损数量
+    slightInjury        : 40    ,  //轻伤数量
+    seriousInjury       : 20   ,  //重伤数量
+    death                : 10              //死亡数量
 };
+
+setMarker(markerOptions);
+function setMarker(options) {
+
+    if (mapMarkers == null || mapMarkers.length == 0) {
+        mapMarkers = [];
+    }
+    //获取展示内容
+    var content;
+    console.log("ok-----setMarker:type"+options.type);
+    if (options.type == 0) {
+        content = setIntersectionContent(options.accidentNumbers,options.accidentAllNumbers,options.name,options.isChenDu,options.showType);
+    }else {
+        content = setRoadContent(options.accidentNumbers,options.accidentAllNumbers,options.name);
+    }
+    console.log("ok-----setMarker")
+    var marker = new AMap.Marker({
+        map: options.map,
+        position: [options.lat,options.lng],
+        icon: new AMap.Icon({
+            size: new AMap.Size(40, 50),  //图标大小
+        }),
+        content:content
+    });
+    console.log("ok-----push")
+    mapMarkers.push(marker);
+
+}
 function addMarker(amap,markerName,position,type,number){
     var Markers = [];
 
     if (mapMarkers == null || mapMarkers.length == 0) {
         mapMarkers = [];
     }
+
     //添加点标记，并使用自己的icon
     var testNumbers = 10;//测试数据，不是则设置为1
     for (var i =0;i<testNumbers;i++) {
@@ -107,7 +175,7 @@ function addMarker(amap,markerName,position,type,number){
         }else {
             content = setRoadContent(number,200,markerName);
         }
-
+        content = testContent();
         var marker = new AMap.Marker({
             map: amap,
             position: position,
@@ -125,6 +193,89 @@ function addMarker(amap,markerName,position,type,number){
     }
  //   showChart();
 
+}
+
+function setIntersectionContent(number,allNumbers,name,isChenDu,showType) {
+
+    var content = "<div style=\"height: 150px;width: 100px\">\n" +
+        "<div  style=\"margin: auto;width: "+getHeight(number,allNumbers)/2+"px;height: "+getHeight(number,allNumbers)/2+"px;background: "+getColor(number,allNumbers)+";border-radius:50%\"></div>" +
+        "    <p style=\"width:100%;margin: auto;text-align: center;color:white;\">事故数："+number+"</p>\n" +
+        "    <p style=\"width:100%;margin: auto;background:white;text-align: center;\">"+name+"</p>\n" +
+        "</div>";
+    //alert("ok");
+    console.log("ok-----setIntersectionContent")
+    if (isChenDu) {
+        var indexs = [0,0,0,0] ;
+        var content2 ="<div class=\"barcontain\">\n" +
+            "         <div class=\"barchild\">\n" +
+            "            <p style=\"margin:auto;text-align: center;\">111</p>\n" +
+            "            <div style=\"margin:auto;width:30px;height:100px;background: red;\"></div>\n" +
+            "            <p style=\"margin:auto;text-align: center;\">总数</p>\n" +
+            "         </div>\n" +
+            "         <div class=\"barchild\" style=\"left: 40px;\">\n" +
+            "            <p style=\"margin:auto;text-align: center;\">111</p>\n" +
+            "            <div style=\"margin:auto;width:30px;height:50px;background: green;\"></div>\n" +
+            "            <p style=\"margin:auto;text-align: center;\">财损</p>\n" +
+            "         </div>\n" +
+            "         <div class=\"barchild\" style=\"left: 80px;\">\n" +
+            "            <p style=\"margin:auto;text-align: center;\">111</p>\n" +
+            "            <div style=\"margin:auto;width:30px;height:70px;background: blue;\"></div>\n" +
+            "            <p style=\"margin:auto;text-align: center;\">轻伤</p>\n" +
+            "         </div>\n" +
+            "         <div class=\"barchild\" style=\"left: 120px;\">\n" +
+            "            <p style=\"margin:auto;text-align: center;\">111</p>\n" +
+            "            <div style=\"margin:auto;width:30px;height:50px;background: yellow;\"></div>\n" +
+            "            <p style=\"margin:auto;text-align: center;\">重伤</p>\n" +
+            "         </div>\n" +
+            "         <div class=\"barchild\" style=\"left: 160px;\">\n" +
+            "            <p style=\"margin:auto;text-align: center;\">111</p>\n" +
+            "            <div style=\"margin:auto;width:30px;height:80px;background: olive;\"></div>\n" +
+            "            <p style=\"margin:auto;text-align: center;\">死亡</p>\n" +
+            "         </div>\n" +
+            "      </div>"
+        return content2;
+    }
+    return content;
+}
+
+function setRoadContent (number,allNumbers,name) {
+    var content = "<div style=\"height: 200px;width: 100px\">\n" +
+        "    <div id=\"bar\" style=\"width: 20px;height: "+getHeight(number,allNumbers)+"px;background: "+getColor(number,allNumbers)+"; margin: auto\"></div>\n" +
+        "    <p style=\"width:100%;margin: auto;text-align: center;color: "+"white"+"\">事故数："+number+"</p>\n" +
+        "    <p style=\"width:100%;margin: auto;background:white;text-align: center;\">"+name+"</p>\n" +
+        "</div>";
+    return content;
+};
+
+function testContent() {
+    var content ="<div class=\"barcontain\">\n" +
+        "         <div class=\"barchild\">\n" +
+        "            <p style=\"margin:auto;text-align: center;\">111</p>\n" +
+        "            <div style=\"margin:auto;width:30px;height:100px;background: red;\"></div>\n" +
+        "            <p style=\"margin:auto;text-align: center;\">总数</p>\n" +
+        "         </div>\n" +
+        "         <div class=\"barchild\" style=\"left: 40px;\">\n" +
+        "            <p style=\"margin:auto;text-align: center;\">111</p>\n" +
+        "            <div style=\"margin:auto;width:30px;height:50px;background: green;\"></div>\n" +
+        "            <p style=\"margin:auto;text-align: center;\">财损</p>\n" +
+        "         </div>\n" +
+        "         <div class=\"barchild\" style=\"left: 80px;\">\n" +
+        "            <p style=\"margin:auto;text-align: center;\">111</p>\n" +
+        "            <div style=\"margin:auto;width:30px;height:70px;background: blue;\"></div>\n" +
+        "            <p style=\"margin:auto;text-align: center;\">轻伤</p>\n" +
+        "         </div>\n" +
+        "         <div class=\"barchild\" style=\"left: 120px;\">\n" +
+        "            <p style=\"margin:auto;text-align: center;\">111</p>\n" +
+        "            <div style=\"margin:auto;width:30px;height:50px;background: yellow;\"></div>\n" +
+        "            <p style=\"margin:auto;text-align: center;\">重伤</p>\n" +
+        "         </div>\n" +
+        "         <div class=\"barchild\" style=\"left: 160px;\">\n" +
+        "            <p style=\"margin:auto;text-align: center;\">111</p>\n" +
+        "            <div style=\"margin:auto;width:30px;height:80px;background: olive;\"></div>\n" +
+        "            <p style=\"margin:auto;text-align: center;\">死亡</p>\n" +
+        "         </div>\n" +
+        "      </div>"
+    return content;
 }
 /*
     * 返回颜色，红黄蓝绿
