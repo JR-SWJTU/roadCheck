e686d8d800e9d71180bc669b968b807e13abed9c
JR-SWJTU <752258828@qq.com>
2017-11-01 17:29:48 +0800

    [add] 添加定时任务，自动删除生成的excel文件

--- src/main/java/com/swjtu/roadCheck/timedTask/ExcelDelete.java
+++ src/main/java/com/swjtu/roadCheck/timedTask/ExcelDelete.java
@@ -18,16 +18,46 @@ public class ExcelDelete {
     //定时任务
     public void execute() throws Exception{
 
-        //File既可以代表文件也可以代表目录
-        File temp = new File(rootPath);
-        //如果目录不存在
-        if (temp.exists()) {
-
-        }
+        deleteDir(new File(rootPath));
 
         System.out.println("定时任务执行了");
 
         _log.info("定时任务删除excel文件夹: " + new Date());
+    }
+
+    /**
+     * 删除空目录
+     * @param dir 将要删除的目录路径
+     */
+    private static void doDeleteEmptyDir(String dir) {
+        boolean success = (new File(dir)).delete();
+        if (success) {
+            System.out.println("Successfully deleted empty directory: " + dir);
+        } else {
+            System.out.println("Failed to delete empty directory: " + dir);
+        }
+    }
+
+    /**
+     * 递归删除目录下的所有文件及子目录下所有文件
+     * @param dir 将要删除的文件目录
+     * @return boolean Returns "true" if all deletions were successful.
+     *                 If a deletion fails, the method stops attempting to
+     *                 delete and returns "false".
+     */
+    private  boolean deleteDir(File dir) {
+        if (dir.isDirectory()) {
+            String[] children = dir.list();
+            //递归删除目录中的子目录下
+            for (int i=0; i<children.length; i++) {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+            }
+        }
+        // 目录此时为空，可以删除
+        return dir.delete();
     }
 
 }
